9ee8015285d599b30d7b6af947e4d8a066d3918d
Orlando Trejo <trejo10@gmail.com>
2020-03-06 20:22:01 -0500

    updated basic_algorithms.py

--- Exercises/basic_algorithms.py
+++ Exercises/basic_algorithms.py
@@ -1,7 +1,7 @@
 # @Author: otrejo
 # @Date:   2020-03-05T17:23:35-05:00
 # @Last modified by:   otrejo
-# @Last modified time: 2020-03-05T17:48:27-05:00
+# @Last modified time: 2020-03-06T20:20:57-05:00
 
 # Recursive binary search using recursion
 def binary_search_recursive(array, target):
@@ -31,3 +31,131 @@ def binary_search_recursive_soln(array, target, start_index, end_index):
         return binary_search_recursive_soln(array, target, start_index, mid_index - 1)
     else:
         return binary_search_recursive_soln(array, target, mid_index + 1, end_index)
+
+# Test function for binary search recursion function
+def test_function(test_case):
+    answer = binary_search_recursive(test_case[0], test_case[1])
+    if answer == test_case[2]:
+        print('Pass!')
+    else:
+        print('Fail!')
+
+# Test case
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+target = 4
+index = 4
+test_case = [array, target, index]
+test_function(test_case) # Should return "Pass!"
+
+# Variation on binary search
+def recursive_binary_search(target, source, left=0):
+    if len(source) == 0:
+        return None
+    center = (len(source) - 1) // 2
+    if source[center] == target:
+        return center + left
+    elif source[center] < target:
+        return recursive_binary_search(target, source[center+1:], left+center+1)
+    else:
+        return recursive_binary_search(target, source[:center], left)
+
+# Find first instance
+def find_first(target, source):
+    i_pos = recursive_binary_search(target, source)
+    if not i_pos:
+        return -1
+    while source[i_pos] == target:
+        n_pos = i_pos - 1
+        if i_pos == 0:
+            return 0
+        elif source[n_pos] == target:
+            i_pos = n_pos
+        else:
+            return i_pos
+
+# Test find first
+multiple = [1, 3, 5, 7, 7, 7, 8, 11, 12, 13, 14, 15]
+print(find_first(7, multiple)) # Should return 3
+print(find_first(9, multiple)) # Should return None
+
+# Determine if source contains target
+def contains(target, source):
+    if recursive_binary_search(target, source) is None:
+        return False
+    else:
+        return True
+
+# Test contains
+letters = ['a', 'c', 'd', 'f', 'g']
+print(contains('a', letters)) ## True
+print(contains('b', letters)) ## False
+
+# Find last instance
+def find_last(target, source):
+    i_pos = recursive_binary_search(target, source)
+    if not i_pos:
+        return -1
+    while source[i_pos] == target:
+        n_pos = i_pos + 1
+        if i_pos == len(source):
+            return 0
+        elif source[n_pos] == target:
+            i_pos = n_pos
+        else:
+            return i_pos
+
+# Find first and last index of target in source
+def first_and_last_index(arr, number):
+    """
+    Given a sorted array that may have duplicate values, use binary
+    search to find the first and last indexes of a given value.
+
+    Args:
+        arr(list): Sorted array (or Python list) that may have duplicate values
+        number(int): Value to search for in the array
+    Returns:
+        a list containing the first and last indexes of the given value
+    """
+
+    # TODO: Write your first_and_last function here
+    # Note that you may want to write helper functions to find the start
+    # index and the end index
+
+    return [find_first(number, arr), find_last(number, arr)]
+
+# Test function
+def test_function(test_case):
+    input_list = test_case[0]
+    number = test_case[1]
+    solution = test_case[2]
+    output = first_and_last_index(input_list, number)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+input_list = [1]
+number = 1
+solution = [0, 0]
+print(find_first(number, input_list))
+print(find_last(number, input_))
+test_case_1 = [input_list, number, solution]
+test_function(test_case_1)
+
+input_list = [0, 1, 2, 3, 3, 3, 3, 4, 5, 6]
+number = 3
+solution = [3, 6]
+test_case_2 = [input_list, number, solution]
+test_function(test_case_2)
+
+input_list = [0, 1, 2, 3, 4, 5]
+number = 5
+solution = [5, 5]
+test_case_3 = [input_list, number, solution]
+test_function(test_case_3)
+
+input_list = [0, 1, 2, 3, 4, 5]
+number = 6
+solution = [-1, -1]
+test_case_4 = [input_list, number, solution]
+test_function(test_case_4)
