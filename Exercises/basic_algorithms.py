f3ac30f0344bca9def90ec97bc85f5723a0f7a4e
Orlando Trejo <trejo10@gmail.com>
2020-03-07 23:51:59 -0500

    added the get_start_index function

--- Exercises/basic_algorithms.py
+++ Exercises/basic_algorithms.py
@@ -1,7 +1,7 @@
 # @Author: otrejo
 # @Date:   2020-03-05T17:23:35-05:00
 # @Last modified by:   otrejo
-# @Last modified time: 2020-03-06T20:20:57-05:00
+# @Last modified time: 2020-03-07T23:51:11-05:00
 
 # Recursive binary search using recursion
 def binary_search_recursive(array, target):
@@ -90,19 +90,26 @@ letters = ['a', 'c', 'd', 'f', 'g']
 print(contains('a', letters)) ## True
 print(contains('b', letters)) ## False
 
-# Find last instance
-def find_last(target, source):
-    i_pos = recursive_binary_search(target, source)
-    if not i_pos:
+# Find start index 
+def find_start_index(arr, number, start_index, end_index):
+    # binary search solution to search for the first index of the array
+    if start_index > end_index:
         return -1
-    while source[i_pos] == target:
-        n_pos = i_pos + 1
-        if i_pos == len(source):
-            return 0
-        elif source[n_pos] == target:
-            i_pos = n_pos
+
+    mid_index = start_index + (end_index - start_index)//2
+
+    if arr[mid_index] == number:
+        current_start_pos = find_start_index(arr, number, start_index, mid_index - 1)
+        if current_start_pos != -1:
+            start_pos = current_start_pos
         else:
-            return i_pos
+            start_pos = mid_index
+        return start_pos
+
+    elif arr[mid_index] < number:
+        return find_start_index(arr, number, mid_index + 1, end_index)
+    else:
+        return find_start_index(arr, number, start_index, mid_index - 1)
 
 # Find first and last index of target in source
 def first_and_last_index(arr, number):
