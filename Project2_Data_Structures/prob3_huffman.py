0a60768d78c9737758d1c154357e73fc030da337
Orlando Trejo <trejo10@gmail.com>
2020-03-23 22:33:39 -0400

    finished problem 3 code and explanation

--- Project2_Data_Structures/prob3_huffman.py
+++ Project2_Data_Structures/prob3_huffman.py
@@ -1,27 +1,71 @@
+# @Author: otrejo
+# @Date:   2020-03-05T00:12:05-05:00
+# @Last modified by:   otrejo
+# @Last modified time: 2020-03-23T22:27:08-04:00
+
+
+
 import sys
 
 def huffman_encoding(data):
-    dict = {}
+    # Make frequency dictionary
+    d = {}
     for char in data:
-        if char in dict:
-            dict[char] += 1
+        if char in d:
+            d[char] += 1
         else:
-            dict[char] = 1
+            d[char] = 1
 
-    l_tuples = sorted(dict.items(), key=lambda tup: tup[1], reverse=True)
+    # Make ordered list of tuples
+    l_tuples = sorted(d.items(), key=lambda tup: tup[1], reverse=True)
+
+    # Make tree
+    t = {}
+    base = '1'
     for tup in l_tuples:
-        print(tup)
+        t[tup[0]] = base
+        base = '0' + base
+
+    # Encode data
+    encoded_data = ''
+    for char in data:
+        encoded_data += t[char]
+
+    return encoded_data, t
 
 def huffman_decoding(data,tree):
-    pass
+    # Flip tree dictionary
+    d = {}
+    for key in tree:
+        d[tree[key]] = key
+    print(d)
+    # Decode data
+    base = ''
+    decoded_data = ''
+    for char in data:
+        if char == '1':
+            decoded_data += d[base + char]
+            base = ''
+        else:
+            base += char
+
+    return decoded_data
+
 
 if __name__ == "__main__":
     codes = {}
 
+    a_great_sentence = "The bird is the word"
 
-a_great_sentence = "The bird is the word"
+    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+    print ("The content of the data is: {}\n".format(a_great_sentence))
 
-print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
-print ("The content of the data is: {}\n".format(a_great_sentence))
+    encoded_data, tree = huffman_encoding(a_great_sentence)
 
-huffman_encoding(a_great_sentence)
+    print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    print ("The content of the encoded data is: {}\n".format(encoded_data))
+
+    decoded_data = huffman_decoding(encoded_data, tree)
+
+    print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+    print ("The content of the encoded data is: {}\n".format(decoded_data))
