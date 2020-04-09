42d43110f99cfbf1cdbdaf5b7782f2f2e22f8f2c
OT <trejo10@gmail.com>
2020-04-08 22:12:34 -0400

    Add problem 2 code.

---
+++ Project3_Problems_Algorithms/prob2_search_array.py
@@ -0,0 +1,41 @@
+#@Author: littlebigchamps
+#@Date:   2020-04-08T21:23:48-04:00
+# @Last modified by:   littlebigchamps
+# @Last modified time: 2020-04-08T22:11:58-04:00
+
+def rotated_array_search(input_list, number):
+    """
+    Find the index by searching in a rotated sorted array
+
+    Args:
+       input_list(array), number(int): Input array to search and the target
+    Returns:
+       int: Index or -1
+    """
+    mid_index = len(input_list) // 2
+    left_side = input_list[0:mid_index]
+    right_side = input_list[mid_index:]
+    if number >= left_side[0]:
+        return linear_search(left_side, number)
+    else:
+        return linear_search(right_side, number) + len(left_side)
+
+def linear_search(input_list, number):
+    for index, element in enumerate(input_list):
+        if element == number:
+            return index
+    return -1
+
+def test_function(test_case):
+    input_list = test_case[0]
+    number = test_case[1]
+    if linear_search(input_list, number) == rotated_array_search(input_list, number):
+        print("Pass")
+    else:
+        print("Fail")
+
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
+test_function([[6, 7, 8, 1, 2, 3, 4], 8])
+test_function([[6, 7, 8, 1, 2, 3, 4], 1])
+test_function([[6, 7, 8, 1, 2, 3, 4], 10])
