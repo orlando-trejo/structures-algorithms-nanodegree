8aba623869d9cf0c65c0156242267d40bcbb4433
Orlando Trejo <trejo10@gmail.com>
2020-04-12 23:22:07 -0400

    Add dummy sort algorithm to problem 2 code.

--- Project3_Problems_Algorithms/prob3_rearrange_digits.py
+++ Project3_Problems_Algorithms/prob3_rearrange_digits.py
@@ -1,8 +1,18 @@
 # @Author: otrejo
 # @Date:   2020-04-11T23:28:21-04:00
 # @Last modified by:   otrejo
-# @Last modified time: 2020-04-11T23:28:37-04:00
+# @Last modified time: 2020-04-12T23:21:45-04:00
 
+
+def bubble_sort_1(l):
+    # TODO: Implement bubble sort solution
+    for i in range(len(l)):
+        for j in range(1, len(l)):
+            if l[j-1] < l[j]:
+                o_j = l[j]
+                l[j] = l[j-1]
+                l[j-1] = o_j
+    return l
 
 
 def rearrange_digits(input_list):
@@ -14,7 +24,16 @@ def rearrange_digits(input_list):
     Returns:
        (int),(int): Two maximum sums
     """
-    pass
+    sorted_list = bubble_sort_1(input_list)
+    num1 = ''
+    num2 = ''
+    for i in range(len(input_list)):
+        if i % 2 == 0:
+            num1 += str(input_list[i])
+        else:
+            num2 += str(input_list[i])
+
+    return [int(num1), int(num2)]
 
 def test_function(test_case):
     output = rearrange_digits(test_case[0])
@@ -24,5 +43,8 @@ def test_function(test_case):
     else:
         print("Fail")
 
+print(rearrange_digits([1, 2, 3, 4, 5]))
+
 test_function([[1, 2, 3, 4, 5], [542, 31]])
 test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+test_function(test_case)
