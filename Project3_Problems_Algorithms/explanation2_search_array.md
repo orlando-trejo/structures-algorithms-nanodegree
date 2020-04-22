aabbcf1cdabc04d0b56b6ec0c9b0c5ce50cbf378
Orlando Trejo <trejo10@gmail.com>
2020-04-21 22:57:26 -0400

    Add test cases and algorithm explanation to problem 2

--- Project3_Problems_Algorithms/explanation2_search_array.md
+++ Project3_Problems_Algorithms/explanation2_search_array.md
@@ -8,5 +8,8 @@
 
 
 # Explanation for problem 2
-The data structure used was a list to keep track of indices.
+The data structure used was a list to keep track of indices and maintain
+the data structure of the input. The algorithm then uses a binary search
+step to divide the input list into the two ordered sub lists. Then the
+algorithm used the provided linear search function to get the index of interest.
 The time complexity is O(log(n)) and space complexity is O(n).
--- Project3_Problems_Algorithms/prob2_search_array.py
+++ Project3_Problems_Algorithms/prob2_search_array.py
@@ -1,7 +1,7 @@
 #@Author: littlebigchamps
 #@Date:   2020-04-08T21:23:48-04:00
-# @Last modified by:   littlebigchamps
-# @Last modified time: 2020-04-08T22:11:58-04:00
+# @Last modified by:   otrejo
+# @Last modified time: 2020-04-21T22:57:04-04:00
 
 def rotated_array_search(input_list, number):
     """
@@ -12,6 +12,10 @@ def rotated_array_search(input_list, number):
     Returns:
        int: Index or -1
     """
+    # Base case
+    if len(input_list) == 0:
+        return -1
+
     mid_index = len(input_list) // 2
     left_side = input_list[0:mid_index]
     right_side = input_list[mid_index:]
@@ -34,8 +38,11 @@ def test_function(test_case):
     else:
         print("Fail")
 
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 8])
-test_function([[6, 7, 8, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6]) # Pass
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1]) # Pass
+test_function([[6, 7, 8, 1, 2, 3, 4], 8]) # Pass
+test_function([[6, 7, 8, 1, 2, 3, 4], 1]) # Pass
+test_function([[6, 7, 8, 1, 2, 3, 4], 10]) # Pass
+# Edge cases
+test_function([[], 10]) # Pass
+test_function([[1, 1, 1, 1, 1, 1, 1], 1]) # Pass
