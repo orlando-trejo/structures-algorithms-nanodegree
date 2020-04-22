8c988ab107bb902a2c5b1d6d7a12561ece5f9af2
Orlando Trejo <trejo10@gmail.com>
2020-04-21 22:30:16 -0400

    Add edge case to problem 1

--- Project3_Problems_Algorithms/prob1_square_root.py
+++ Project3_Problems_Algorithms/prob1_square_root.py
@@ -1,7 +1,7 @@
 # @Author: littlebigchamps
 # @Date:   2020-04-08T21:15:05-04:00
-# @Last modified by:   littlebigchamps
-# @Last modified time: 2020-04-08T21:22:26-04:00
+# @Last modified by:   otrejo
+# @Last modified time: 2020-04-21T22:30:02-04:00
 
 def sqrt(number):
     """
@@ -22,8 +22,12 @@ def sqrt(number):
     else:
         return index
 
-print ("Pass" if  (3 == sqrt(9)) else "Fail")
-print ("Pass" if  (0 == sqrt(0)) else "Fail")
-print ("Pass" if  (4 == sqrt(16)) else "Fail")
-print ("Pass" if  (1 == sqrt(1)) else "Fail")
-print ("Pass" if  (5 == sqrt(27)) else "Fail")
+print ("Pass" if  (3 == sqrt(9)) else "Fail") # Pass
+print ("Pass" if  (0 == sqrt(0)) else "Fail") # Pass
+print ("Pass" if  (4 == sqrt(16)) else "Fail") # Pass
+print ("Pass" if  (1 == sqrt(1)) else "Fail") # Pass
+print ("Pass" if  (5 == sqrt(27)) else "Fail") # Pass
+print ("Pass" if  (5 == sqrt(24)) else "Fail") # Fail
+# Edge cases
+print ("Pass" if  (1000 == sqrt(1000000)) else "Fail") # Edge 1 - large - Pass
+print ("Pass" if  (0 == sqrt(0)) else "Fail") # Edge 2 - small - Pass
