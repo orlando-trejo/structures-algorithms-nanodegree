e9dfab0afab12ed9f97664eddda5e80628016f3b
Orlando Trejo <trejo10@gmail.com>
2020-04-22 21:29:41 -0400

    Update code to problem 2

--- Project3_Problems_Algorithms/prob2_search_array.py
+++ Project3_Problems_Algorithms/prob2_search_array.py
@@ -1,7 +1,7 @@
 #@Author: littlebigchamps
 #@Date:   2020-04-08T21:23:48-04:00
 # @Last modified by:   otrejo
-# @Last modified time: 2020-04-21T22:57:04-04:00
+# @Last modified time: 2020-04-22T21:29:23-04:00
 
 def rotated_array_search(input_list, number):
     """
@@ -12,17 +12,30 @@ def rotated_array_search(input_list, number):
     Returns:
        int: Index or -1
     """
+    return rotated_array_recursion(input_list, number, 0, len(input_list)-1)
+
+def rotated_array_recursion(input_list, number, start_index, end_index):
+    '''
+
+    Args:
+        input_list(array), number(int), start_index (int), end_index (int)
+    Returns:
+        int: Index or -1
+    '''
     # Base case
-    if len(input_list) == 0:
+    if start_index > end_index:
         return -1
 
-    mid_index = len(input_list) // 2
-    left_side = input_list[0:mid_index]
-    right_side = input_list[mid_index:]
-    if number >= left_side[0]:
-        return linear_search(left_side, number)
-    else:
-        return linear_search(right_side, number) + len(left_side)
+    mid_index = start_index + (end_index - start_index) // 2
+    mid_element = input_list[mid_index]
+
+    if mid_element == number:
+        return mid_index
+
+    left_index = rotated_array_recursion(input_list, number, start_index, mid_index-1)
+    right_index = rotated_array_recursion(input_list, number, mid_index+1, end_index)
+
+    return max(left_index, right_index)
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):
