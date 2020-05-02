9e0ca45483d0461d73e4c4eb59134e63b96cf54b
Orlando Trejo <trejo10@gmail.com>
2020-05-02 17:54:25 -0400

    Update problem 2 code and explanation

--- Project3_Problems_Algorithms/explanation2_search_array.md
+++ Project3_Problems_Algorithms/explanation2_search_array.md
@@ -11,5 +11,9 @@
 The data structure used is a list to keep track of indices and maintain
 the data structure of the input. The algorithm chosen is a binary search
 tree with recursion in order to minimize the number of passes needed
-to find the number.
+to find the number. The way this algorithm works is by checking if the
+left or to the right of the mid element is increasing, and if the
+target number lies between either side. If it does, then explore that side
+further. If not, then explore the other side with recursion.
+
 The time complexity is O(log(n)) and space complexity is O(n).
--- Project3_Problems_Algorithms/prob2_search_array.py
+++ Project3_Problems_Algorithms/prob2_search_array.py
@@ -1,7 +1,7 @@
 #@Author: littlebigchamps
 #@Date:   2020-04-08T21:23:48-04:00
 # @Last modified by:   otrejo
-# @Last modified time: 2020-04-29T23:51:00-04:00
+# @Last modified time: 2020-05-02T17:46:29-04:00
 
 def rotated_array_search(input_list, number):
     """
@@ -22,19 +22,30 @@ def rotated_array_recursion(input_list, number, start_index, end_index):
     Returns:
         int: Index or -1
     '''
+    print(input_list[start_index:end_index+1])
     # Base case
     if start_index > end_index:
         return -1
 
     mid_index = start_index + (end_index - start_index) // 2
     mid_element = input_list[mid_index]
-
+    print(mid_element)
     if mid_element == number:
         return mid_index
 
-    if input_list[start_index]:
+
+    if input_list[start_index] <= mid_element:
+        if input_list[start_index] <= number and number < mid_element:
+            return rotated_array_recursion(input_list, number, start_index, mid_index-1)
+        else:
+            return rotated_array_recursion(input_list, number, mid_index+1, end_index)
+
+    elif mid_element < number and number <= input_list[end_index]:
+        return rotated_array_recursion(input_list, number, mid_index+1, end_index)
+
     else:
-        return
+        return rotated_array_recursion(input_list, number, start_index, mid_index)
+
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):
@@ -57,4 +68,4 @@ test_function([[6, 7, 8, 1, 2, 3, 4], 1]) # Pass
 test_function([[6, 7, 8, 1, 2, 3, 4], 10]) # Pass
 # Edge cases
 test_function([[], 10]) # Pass
-test_function([[1, 1, 1, 1, 1, 1, 1], 1]) # Pass
+test_function([[1, 1, 1, 1, 1, 1, 1], 1]) # Fail
