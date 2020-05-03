3b3c9a5dc193bd2e7c745fda148971e4adc6b540
Orlando Trejo <trejo10@gmail.com>
2020-05-02 20:20:53 -0400

    Update code and explanation for problem 4

--- Project3_Problems_Algorithms/explanation4_dutch_flag.md
+++ Project3_Problems_Algorithms/explanation4_dutch_flag.md
@@ -8,5 +8,9 @@
 
 
 # Explanation for problem 4
-Used lists to keep track of indices.
+Used lists to keep track of indices. The algorithm used to sort the numbers
+consists of a while loop that moves 0s to the left side of the list, 2s to the
+right side of the list, and keeps the 1s in place. This allows to not use extra
+space and run through the algorithm in O(n).
+
 Time and space complexity is O(n).
--- Project3_Problems_Algorithms/prob4_dutch_flag.py
+++ Project3_Problems_Algorithms/prob4_dutch_flag.py
@@ -1,7 +1,7 @@
 # @Author: otrejo
 # @Date:   2020-04-11T23:47:12-04:00
 # @Last modified by:   otrejo
-# @Last modified time: 2020-04-18T22:58:51-04:00
+# @Last modified time: 2020-05-02T20:18:38-04:00
 
 
 
@@ -12,18 +12,24 @@ def sort_012(input_list):
     Args:
        input_list(list): List to be sorted
     """
-    zeros = []
-    ones = []
-    twos = []
-    for i in input_list:
-        if i == 0:
-            zeros.append(0)
-        elif i == 1:
-            ones.append(1)
-        else:
-            twos.append(2)
+    next_0_index = 0
+    next_2_index = len(input_list) - 1
+    moving_index = 0
 
-    return zeros + ones + twos
+    while (moving_index <= next_2_index):
+        if input_list[moving_index] == 0:
+            input_list[moving_index] = input_list[next_0_index]
+            input_list[next_0_index] = 0
+            moving_index += 1
+            next_0_index += 1
+        elif input_list[moving_index] == 2:
+            input_list[moving_index] = input_list[next_2_index]
+            input_list[next_2_index] = 2
+            next_2_index -= 1
+        else:
+            moving_index += 1
+
+    return input_list
 
 def test_function(test_case):
     sorted_array = sort_012(test_case)
@@ -35,3 +41,8 @@ def test_function(test_case):
 test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2]) #Pass
 test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1]) #Pass
 test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2]) #Pass
+
+# Edge cases
+test_function([0, 0, 0, 0, 0, 0]) #Pass
+test_function([2, 2, 2, 2, 2, 2]) #Pass
+test_function([]) #Pass
