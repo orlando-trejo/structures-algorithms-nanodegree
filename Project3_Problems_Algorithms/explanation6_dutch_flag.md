886a023261f26d33bbe752f00285077bad2fdd41
Orlando Trejo <trejo10@gmail.com>
2020-05-02 20:31:27 -0400

    Update code and explanation for problem 6

--- Project3_Problems_Algorithms/explanation6_dutch_flag.md
+++ Project3_Problems_Algorithms/explanation6_dutch_flag.md
@@ -8,5 +8,12 @@
 
 
 # Explanation for problem 6
-Used lists to keep track of indices.
+Used lists to keep track of indices. This algorithm checks that input list
+has length 0. If so, it returns 'None'. If not, then it continues to determine
+initial values for the minimum and maximum values. Then it proceeds along the
+list to compare the minimum and maximum current values. If the current value is
+lower than the current minimum value, then it becomes the new minimum value.
+Similarly, if the current values is larger than the maximum value, then it 
+becomes the new maximum value.
+
 Time complexity is O(n) and space complexity is O(2).
--- Project3_Problems_Algorithms/prob6_unsorted_array.py
+++ Project3_Problems_Algorithms/prob6_unsorted_array.py
@@ -1,7 +1,7 @@
 # @Author: otrejo
 # @Date:   2020-04-16T23:49:35-04:00
 # @Last modified by:   otrejo
-# @Last modified time: 2020-04-18T23:01:11-04:00
+# @Last modified time: 2020-05-02T20:28:39-04:00
 
 
 
@@ -13,22 +13,25 @@ def get_min_max(ints):
        ints(list): list of integers containing one or more integers
     """
     # Compare first two integers
+    if len(ints) == 0:
+        return None
+
     if ints[0] < ints[1]:
-        min = ints[0]
-        max = ints[1]
+        min_int = ints[0]
+        max_int = ints[1]
     else:
-        min = ints[1]
-        max = ints[0]
+        min_int = ints[1]
+        max_int = ints[0]
 
     for i in range(2, len(ints)):
-        if ints[i] < min:
-            min = ints[i]
-        elif ints[i] > max:
-            max = ints[i]
+        if ints[i] < min_int:
+            min_int = ints[i]
+        elif ints[i] > max_int:
+            max_int = ints[i]
         else:
             continue
 
-    return (min, max)
+    return (min_int, max_int)
 
 ## Example Test Case of Ten Integers
 import random
@@ -41,4 +44,10 @@ print ("Pass" if ((0, 8) == get_min_max(l)) else "Fail") # Fail
 
 l = [0, 0, 0, 0, 0]
 print ("Pass" if ((0, 0) == get_min_max(l)) else "Fail") # Pass
-print ("Pass" if ((0, 1) == get_min_max(l)) else "Fail") # Fail 
+print ("Pass" if ((0, 1) == get_min_max(l)) else "Fail") # Fail
+
+# Edge cases
+l = []
+print ("Pass" if (None == get_min_max(l)) else "Fail") # Pass
+l = [10000, 0.0000001, 1000, 0.01]
+print ("Pass" if ((0.0000001, 10000) == get_min_max(l)) else "Fail") # Pass
