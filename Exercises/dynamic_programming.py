a3da61daee0e298a424ff1ec2383f251c4f53ab0
Orlando Trejo <trejo10@gmail.com>
2020-06-12 19:07:35 -0400

    Add last dynamic programming exercise

--- Exercises/dynamic_programming.py
+++ Exercises/dynamic_programming.py
@@ -1,7 +1,7 @@
 # @Author: otrejo
 # @Date:   2020-05-20T22:24:35-04:00
 # @Last modified by:   otrejo
-# @Last modified time: 2020-06-11T21:55:31-04:00
+# @Last modified time: 2020-06-12T12:39:13-04:00
 
 import collections
 
@@ -119,4 +119,99 @@ test_function(test_case)
 string = 'BANANO'
 solution = 3
 test_case = [string, solution]
+test_function(test_case)
+
+# The Coin Change Problem
+def coin_change(coins, amount):
+    # Store coins with amounts
+    memo = {}
+
+    # Recursive function to determine change
+    def return_change(remaining):
+        # Base cases
+        if remaining < 0:
+            return float('inf')
+        if remaining == 0:
+            return 0
+
+        # Cehck if previously calculated
+        if remaining not in memo:
+            memo[remaining] = min(return_change(remaining-c)+1 for c in coins)
+
+        return memo[remaining]
+
+    res = return_change(amount)
+
+    return -1 if res == float('inf') else res
+
+def test_function(test_case):
+    arr = test_case[0]
+    amount = test_case[1]
+    solution = test_case[2]
+    output = coin_change(arr, amount)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+arr = [1,2,5]
+amount = 11
+solution = 3
+test_case = [arr, amount, solution]
+test_function(test_case)
+
+
+# Stock Prices
+def max_returns(prices):
+    """
+    Calculate maxiumum possible return
+
+    Args:
+       prices(array): array of prices
+    Returns:
+       int: The maximum profit possible
+    """
+    min_price_index = 0
+    max_price_index = 1
+    current_min_price_index = 0
+
+    if len(prices) < 2:
+        return
+
+    for i in range(1, len(prices)):
+        if prices[i] < prices[current_min_price_index]:
+            current_min_price_index = i
+
+        if prices[max_price_index] - prices[min_price_index] < prices[i] - prices[current_min_price_index]:
+            max_price_index = i
+            min_price_index = current_min_price_index
+
+    max_profit = prices[max_price_index] - prices[min_price_index]
+
+    return max_profit
+
+# Test Cases
+def test_function(test_case):
+    prices = test_case[0]
+    solution = test_case[1]
+    output = max_returns(prices)
+    if output == solution:
+        print("Pass Profits")
+    else:
+        print("Fail")
+
+
+prices = [2, 2, 7, 9, 9, 12, 18, 23, 34, 37, 45, 54, 78]
+solution = 76
+test_case = [prices, solution]
+test_function(test_case)
+
+prices = [54, 18, 37, 9, 11, 48, 23, 1, 7, 34, 2, 45, 67]
+solution = 66
+test_case = [prices, solution]
+test_function(test_case)
+
+prices = [78, 54, 45, 37, 34, 23, 18, 12, 9, 9, 7, 2, 2]
+solution = 0
+test_case = [prices, solution]
 test_function(test_case)
